0efaef5e598655c6575d1346b09f986247b08a93
Eisaichen <chenzhe82807544@hotmail.com>
2025-04-10 00:45:34 -0500

    v2.87 CI

--- 2.87/build.sh
+++ 2.87/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+cd ./2.87
 wget --no-check-certificate --no-cache https://trex-tgn.cisco.com/trex/release/v2.87.tar.gz
 tar -xzvf v2.87.tar.gz
 mv v2.87 ./build/
 chmod +x ../entry.sh
 cp ../entry.sh ./build/v2.87/
 docker build --no-cache --pull -t eisai/cisco-trex:v2.87 ./build
-docker push eisai/cisco-trex:v2.87
-docker rmi eisai/cisco-trex:v2.87
-rm -rf ./build/v2.87
-rm v2.87.tar.gz
+if [ $GH_CI_PUSH -eq "true" ]; then
+    docker push eisai/cisco-trex:v2.87
+fi
+
